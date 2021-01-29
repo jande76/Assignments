8ccdce8dde56ab736786d17e75e61e1ecc92c301
jande76 <44031022+jande76@users.noreply.github.com>
2021-01-29 15:28:58 -0600

    Add files via upload

---
+++ black_jack_update.py
@@ -0,0 +1,96 @@
+from IPython.display import clear_output as co
+import random
+
+#CLASSES
+
+class Game:
+    def __init__(self):
+        pass
+
+    def play(self):
+        playing = True
+
+# Class for card
+class Card:
+    def __init__(self, suit, rank):    
+       self.suit = suit,
+       self.rank = rank
+    def __repr__(self): 
+
+# Class for the deck
+class Deck:
+    def __init__(self,):
+        self.deck = [] 
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+
+#Randomizes the order of the deck list
+    def shuffle(self)  
+        random.shuffle(self.deck)
+
+    # deal function   
+    # def deal(self):
+
+#Class for Dealer
+
+
+class Dealer:
+dealer_cards = []
+    def __init__(self, dealer_cards)
+# function to deal the cards and only show one card at a time
+    while len(dealer_cards) != 2:
+        # picks random integer card between 1 and 13 and adds it to the empty list
+        dealer_cards.append(random, randit(1, 13))
+        if len(dealer_cards) == 2:
+            print("The dealer has _ and: ", dealer_cards[1])
+            continue
+        if sum(dealer_cards) == 21:
+            print("Dealer wins")
+        elif sum(dealer_cards) > 21:
+            print("Dealer busts")
+
+# Class for the player(dealer)
+
+class Player:
+player_cards = []
+   def __init__(self, player_cards)
+       while len(player_cards) != 2:
+            player_cards.append(random, randit(1, 13))
+        if len(player_cards) == 2:
+            print("You have: ", player_cards)
+        continue
+        if sum(player_cards) == 21:
+            print("You win")
+        elif sum(player_cards) > 21:
+            print("You bust")
+        
+        while sum(player_cards) < 2
+            next_move = str(input)("Select 'stay' or 'hit' ").lower()
+                if next_move == "hit":
+                    player_cards.append(random, randit(1, 13))
+                    #show cards
+                    print("Your new hand total is " str(sum(player_cards)) + ", your cards are ", player_cards))
+                else:
+                    print("The dealer has: ", str(sum(dealer_cards))
+                    print("You have: ", str(sum(player_cards)) + ", ", your cards are ", player_cards))
+
+
+suits = ('Clubs', 'Diamonds', 'Spades', 'Hearts ')
+ranks = ('one', 'two', 'three', 'four', 'five', 'six', 'seven',
+         'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen')
+value = {'ace':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,'nine':9, 'ten':10, 'jack':11, 'queen':12, 'king':13, }
+    
+#FUNCTIONS
+   
+#hit or stand
+def hit_stand()
+    pass
+
+def 
+
+(can someone invite me to the zoom?)
+
+
+
+    while not game_Over:
