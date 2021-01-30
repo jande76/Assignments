2adb6ed43ab3d6d7c4d74d31c0d5ec251f6030de
Taty <tatyanaparks@gmail.com>
2021-01-30 10:05:42 -0600

    Changed card values

--- black_jack_update.py
+++ black_jack_update.py
@@ -79,7 +79,7 @@ player_cards = []
 suits = ('Clubs', 'Diamonds', 'Spades', 'Hearts ')
 ranks = ('one', 'two', 'three', 'four', 'five', 'six', 'seven',
          'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen')
-value = {'ace':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,'nine':9, 'ten':10, 'jack':11, 'queen':12, 'king':13, }
+value = {'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8,'nine': 9, 'ten': 10, 'jack': 10, 'queen': 10, 'king': 10, 'ace': 11}
     
 #FUNCTIONS
    
